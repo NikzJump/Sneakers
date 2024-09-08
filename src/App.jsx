b0454145494de6ec0559a2f7171f296fa8b9d1eb
Kirill Sheva <kirillsheva80@gmail.com>
2024-09-08 19:53:58 +0300

    Sneakers cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+          <div className="cart-item d-flex align-center mb-20">
+            <div style={{backgroundImage: 'url(./img/sneakers/1.jpg)'}} className="cart-item-img">
+
+            </div>
+            <div className="m-20">
+              <p className="mb-5">Кроссовки 1</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="remove-btn" src="./img/btn-remove.svg" alt="Remove" />
+          </div>
+          <div className="cart-item d-flex align-center mb-20">
+            <div style={{backgroundImage: 'url(./img/sneakers/2.jpg)'}} className="cart-item-img">
+
+            </div>
+            <div className="m-20">
+              <p className="mb-5">Кроссовки 2</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="remove-btn" src="./img/btn-remove.svg" alt="Remove" />
+          </div>
+        </div>
+      </div>
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img height={40} width={40} src="/img/logo.png" alt="" />
@@ -13,19 +38,28 @@ function App() {
         </div>
         <ul className="d-flex">
           <li className="mr-30">
-            <img height={12} width={18} src="/img/cart.png" alt="" />
+            <img height={12} width={18} src="/img/cart.svg" alt="" />
             <span>1205 руб.</span>
           </li>
           <li>
-            <img height={18} width={18} src="/img/profile.png" alt="" />
+            <img height={18} width={18} src="/img/user.svg" alt="" />
           </li>
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex align-center mb-40 justify-between">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="./img/search.svg" alt="Search" />
+            <input placeholder="Поиск" />
+          </div>
+        </div>
         <div className="d-flex">
           <div className="card">
-            <img width={156} height={112} src="./img/sneakers/sneakers1.png" alt="" />
+            <div className="favorite">
+            <img src="./img/heart.svg" alt="Unliked" />
+            </div>
+            <img width={125} height={112} src="./img/sneakers/1.jpg" alt="" />
             <h5>Кроссовки 1</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
@@ -33,12 +67,12 @@ function App() {
                 <b>12999 руб.</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src="./img/plus.png" alt="Plus" />
+                <img width={11} height={11} src="./img/plus.svg" alt="Plus" />
               </button>
             </div>
           </div>
           <div className="card">
-            <img width={156} height={112} src="./img/sneakers/sneakers2.png" alt="" />
+            <img width={125} height={112} src="./img/sneakers/2.jpg" alt="" />
             <h5>Кроссовки 1</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
@@ -46,12 +80,12 @@ function App() {
                 <b>12999 руб.</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src="./img/plus.png" alt="Plus" />
+                <img width={11} height={11} src="./img/plus.svg" alt="Plus" />
               </button>
             </div>
           </div>
           <div className="card">
-            <img width={156} height={112} src="./img/sneakers/sneakers3.png" alt="" />
+            <img width={125} height={112} src="./img/sneakers/3.jpg" alt="" />
             <h5>Кроссовки 1</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
@@ -59,12 +93,12 @@ function App() {
                 <b>12999 руб.</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src="./img/plus.png" alt="Plus" />
+                <img width={11} height={11} src="./img/plus.svg" alt="Plus" />
               </button>
             </div>
           </div>
           <div className="card">
-            <img width={156} height={112} src="./img/sneakers/sneakers4.png" alt="" />
+            <img width={125} height={112} src="./img/sneakers/4.jpg" alt="" />
             <h5>Кроссовки 1</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
@@ -72,7 +106,7 @@ function App() {
                 <b>12999 руб.</b>
               </div>
               <button className="button">
-                <img width={11} height={11} src="./img/plus.png" alt="Plus" />
+                <img width={11} height={11} src="./img/plus.svg" alt="Plus" />
               </button>
             </div>
           </div>
