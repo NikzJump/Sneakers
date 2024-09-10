13cd60da53898c733e0d47c775455573ed19c0dc
Kirill Sheva <kirillsheva80@gmail.com>
2024-09-11 00:06:40 +0300

    Sneakers props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Card from "./components/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
+const arr = [
+  {name: "Кроссовки 1", price: "12 999 руб.", imgUrl: "./img/sneakers/1.jpg"},
+  {name: "Кроссовки 2", price: "13 499 руб.", imgUrl: "./img/sneakers/2.jpg"},
+  {name: "Кроссовки 3", price: "16 999 руб.", imgUrl: "./img/sneakers/3.jpg"},
+  {name: "Кроссовки 4", price: "11 999 руб.", imgUrl: "./img/sneakers/4.jpg"}
+]
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -16,10 +23,13 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+          {arr.map(sneak => (
+            <Card
+             name={sneak.name}
+             price={sneak.price}
+             imgUrl={sneak.imgUrl}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 
-function Card() {
+var likedStatus = false
+
+function Card({name, price, imgUrl}) {
     return (
         <div className="card">
             <div className="favorite">
                 <img src="./img/heart.svg" alt="Unliked" />
             </div>
-            <img width={125} height={112} src="./img/sneakers/1.jpg" alt="" />
-            <h5>Кроссовки 1</h5>
+            <img width={125} height={112} src={imgUrl} alt="" />
+            <h5>{name}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Цена</span>
-                    <b>12999 руб.</b>
+                    <b>{price}</b>
             </div>
-            <button className="button">
-                <img width={11} height={11} src="./img/plus.svg" alt="Plus" />
+            <button className="button" onClick={() => likedStatus = true}>
+                <img width={11} height={11} src={likedStatus === false ? "./img/plus.svg": "./img/liked.svg"} alt="Plus" />
             </button>
         </div>
       </div>
